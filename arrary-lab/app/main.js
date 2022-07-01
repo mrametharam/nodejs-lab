917e9266c90659add76979557fcb215ac369df32
Martin Rakesh A Metharam <Martin.Metharam@sykes.com>
2022-07-01 16:13:58 +0800

    added array-lab project.

---
+++ arrary-lab/app/main.js
@@ -0,0 +1,9 @@
+let arrSource = [123, 234, 345, 'test', 'hello', 'world'];
+
+const arrNew = arrSource;
+console.log(arrNew === arrSource);
+
+arrSource.push('Kyle!!!');
+arrSource = [];
+
+console.log(arrNew);
